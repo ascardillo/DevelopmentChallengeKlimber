5dc123e6c7c12c5d4e46032c06af073f3837920e
ARIEL85\ascar <ascardillo@outlook.com>
2023-10-30 10:06:25 -0300

    se hace correción

--- Source/DevelopmentChallenge.Data.Test2/Services/ReporteServiceNunitTest.cs
+++ Source/DevelopmentChallenge.Data.Test2/Services/ReporteServiceNunitTest.cs
@@ -1,34 +1,43 @@
 ﻿using DevelopmentChallenge.Data.Classes;
 using DevelopmentChallenge.Data.Services;
 using DevelopmentChallenge.Data.Utils;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace UnitTests
+namespace DevelopmentChallenge.Data.Test2.Services
 {
-    [TestClass]
-    public class UnitTest1
+    [TestFixture]
+    public class ReporteServiceNunitTest
     {
-        [TestMethod]
+        private IReporteServices _reporte;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _reporte = new ReporteService();
+        }
+
+        [TestCase]
         public void TestResumenListaVacia()
         {
-            var reporte = new ReporteService();
+            var listaVacia = new List<FormaGeometricaBase>();
 
             Assert.AreEqual("<h1>Lista vacía de formas!</h1>",
-                reporte.Imprimir(new List<FormaGeometricaBase>(), TipoIdioma.Castellano));
+               _reporte.Imprimir(listaVacia, TipoIdioma.Castellano));
         }
 
-        [TestMethod]
+        [TestCase]
         public void TestResumenListaVaciaFormasEnIngles()
         {
-            var reporte = new ReporteService();
-
             Assert.AreEqual("<h1>Empty list of shapes!</h1>",
-                reporte.Imprimir(new List<FormaGeometricaBase>(), TipoIdioma.Ingles));
+                _reporte.Imprimir(new List<FormaGeometricaBase>(), TipoIdioma.Ingles));
         }
 
-        [TestMethod]
+        [TestCase]
         public void TestResumenListaVaciaFormasEnItaliano()
         {
             var reporte = new ReporteService();
@@ -37,34 +46,34 @@ namespace UnitTests
                 reporte.Imprimir(new List<FormaGeometricaBase>(), TipoIdioma.Italiano));
         }
 
-        [TestMethod]
+        [TestCase]
         public void TestResumenListaConUnCuadrado()
         {
             var cuadrados = new List<FormaGeometricaBase> { new Cuadrado(5) };
-            var resumen = new ReporteService().Imprimir(cuadrados, TipoIdioma.Castellano);
+            var resumen = _reporte.Imprimir(cuadrados, TipoIdioma.Castellano);
 
             Assert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 formas Perimetro 20 Area 25", resumen);
         }
 
-        [TestMethod]
+        [TestCase]
         public void TestResumenListaConUnTrapecioRectangulo()
         {
-            var trapeciosRectangulos = new List<FormaGeometricaBase> { new TrapecioRectangulo(5,3,4) };
-            var resumen = new ReporteService().Imprimir(trapeciosRectangulos, TipoIdioma.Castellano);
+            var trapeciosRectangulos = new List<FormaGeometricaBase> { new TrapecioRectangulo(5, 3, 4) };
+            var resumen = _reporte.Imprimir(trapeciosRectangulos, TipoIdioma.Castellano);
 
             Assert.AreEqual("<h1>Reporte de Formas</h1>1 Trapecio rectangulo | Area 16 | Perimetro 16,47 <br/>TOTAL:<br/>1 formas Perimetro 16,47 Area 16", resumen);
         }
 
-        [TestMethod]
+        [TestCase]
         public void TestResumenListaConMasTrapeciosRectangulos()
         {
             var trapeciosRectangulos = new List<FormaGeometricaBase> { new TrapecioRectangulo(5, 3, 4), new TrapecioRectangulo(10, 5, 5) };
-            var resumen = new ReporteService().Imprimir(trapeciosRectangulos, TipoIdioma.Castellano);
+            var resumen = _reporte.Imprimir(trapeciosRectangulos, TipoIdioma.Castellano);
 
             Assert.AreEqual("<h1>Reporte de Formas</h1>2 Trapecios rectangulos | Area 53,5 | Perimetro 43,54 <br/>TOTAL:<br/>2 formas Perimetro 43,54 Area 53,5", resumen);
         }
 
-        [TestMethod]
+        [TestCase]
         public void TestResumenListaConMasCuadrados()
         {
             var cuadrados = new List<FormaGeometricaBase> {
@@ -73,12 +82,12 @@ namespace UnitTests
                 new Cuadrado(3),
             };
 
-            var resumen = new ReporteService().Imprimir(cuadrados, TipoIdioma.Ingles);
+            var resumen = _reporte.Imprimir(cuadrados, TipoIdioma.Ingles);
 
             Assert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35", resumen);
         }
 
-        [TestMethod]
+        [TestCase]
         public void TestResumenListaConMasTiposEnIngles()
         {
             var formas = new List<FormaGeometricaBase> {
@@ -91,14 +100,14 @@ namespace UnitTests
                 new TrianguloEquilatero(4.2m),
             };
 
-            var resumen = new ReporteService().Imprimir(formas, TipoIdioma.Ingles);
+            var resumen = _reporte.Imprimir(formas, TipoIdioma.Ingles);
 
             Assert.AreEqual(
                 "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13,01 | Perimeter 18,06 <br/>3 Triangles | Area 49,64 | Perimeter 51,6 <br/>TOTAL:<br/>7 shapes Perimeter 97,66 Area 91,65",
                 resumen);
         }
 
-        [TestMethod]
+        [TestCase]
         public void TestResumenListaConMasTiposEnCastellano()
         {
             var formas = new List<FormaGeometricaBase> {
@@ -111,7 +120,7 @@ namespace UnitTests
                 new TrianguloEquilatero(4.2m),
             };
 
-            var resumen = new ReporteService().Imprimir(formas, TipoIdioma.Castellano);
+            var resumen = _reporte.Imprimir(formas, TipoIdioma.Castellano);
 
             Assert.AreEqual(
                 "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Circulos | Area 13,01 | Perimetro 18,06 <br/>3 Triangulos | Area 49,64 | Perimetro 51,6 <br/>TOTAL:<br/>7 formas Perimetro 97,66 Area 91,65",
